2a787faabf3c37487401d513aaeb28a03ffc6d62
picaro.vs@gmail.com <>
2012-10-16 10:00:21 +0000

    c2

---
+++ src/main/java/com/op/cookcloud/controller/BarcodeController.java
@@ -0,0 +1,62 @@
+package com.op.cookcloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+import com.op.cookcloud.model.Product;
+//import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+//import javax.xml.transform.Source;
+//import javax.xml.transform.stream.StreamSource;
+
+
+@Controller
+@RequestMapping("/barcode")
+public class BarcodeController {
+
+//	private Jaxb2Marshaller jaxb2Mashaller;
+	
+//	public void setJaxb2Mashaller(Jaxb2Marshaller jaxb2Mashaller) {
+//		this.jaxb2Mashaller = jaxb2Mashaller;
+//	}
+
+
+	@RequestMapping(value="/{code}", method = RequestMethod.GET,headers="Accept=application/xml, application/json")
+	public @ResponseBody Product getMovie(@PathVariable String code, ModelMap model) throws NotFoundException {
+      Product product = new Product();
+      if (code.equals("1")){
+        product.setName("234");
+        return product;
+      }
+    //Try to find product by code from MondoDB
+    
+    //If ok - return Product with seconds
+    
+    //If not ok - look in barcodes DB return Product name 
+    
+    //If not found - return error
+    if (1 == 1) 
+        throw new NotFoundException("Id not found in the request");
+    //Source source = new StreamSource(new StringReader(body));
+		//Employee e = (Employee) jaxb2Mashaller.unmarshal(source);
+
+    
+		return  product;
+;
+
+	}
+ 
+ @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="Id Not Found")
+  public class NotFoundException extends Exception {
+
+    public NotFoundException(String msg) {
+        super(msg);
+    }
+  }
+	
+}
---
+++ src/main/java/com/op/cookcloud/model/Product.java
@@ -0,0 +1,121 @@
+package com.op.cookcloud.model;
+
+public class Product
+{
+
+    private int id;
+    
+    private String name;
+    
+    private String barcode;
+    
+    private String description;
+    
+    private String country;
+    
+    private int countryId;
+    
+    private String imgURL;
+
+    //private String error;
+
+    /**
+     * @return the barcode
+     */
+    public String getBarcode() {
+        return barcode;
+    }
+
+    /**
+     * @param barcode the barcode to set
+     */
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the countryId
+     */
+    public int getCountryId() {
+        return countryId;
+    }
+
+    /**
+     * @param countryId the countryId to set
+     */
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the imgURL
+     */
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    /**
+     * @param imgURL the imgURL to set
+     */
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
