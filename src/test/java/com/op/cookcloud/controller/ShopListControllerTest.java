953a9de65bd222c79603ddcca0a173903f4bc0c9
picaro <picaro.vs@gmail.com>
2013-12-15 14:50:19 +0200

    ShopListControllerTest created

--- src/test/java/com/op/cookcloud/controller/ShopListControllerTest.java
+++ src/test/java/com/op/cookcloud/controller/ShopListControllerTest.java
@@ -12,6 +12,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created with IntelliJ IDEA.
  * User: picaro
@@ -33,8 +38,44 @@ public class ShopListControllerTest extends BaseDaoTest{
         shopList.setNote("aaa");
         shopList.setPerson(createPerson());
         shopListController.create(shopList);
+
         ShopList shopList1 = shopListController.read(1);
+        assertEquals(shopList1.getNote(), "aaa");
         LOG.debug(shopList1.getId());
+    }
+
+    @Test
+    public void deleteShopListsTest(){
+        ShopList shopList = new ShopList();
+        shopList.setNote("aaa");
+        shopList.setPerson(createPerson());
+        shopListController.create(shopList);
+
+        List<ShopList> shopListL = shopListDao.findAll();
+        assertEquals(shopListL.size(),1);
+
+        shopListController.delete(shopListL.get(0).getId());
+
+        assertEquals(shopListDao.findAll().size(), 0);
+    }
+
+    @Test
+    public void updateShopListsTest(){
+        ShopList shopList = new ShopList();
+        shopList.setNote("aaa");
+        shopList.setPerson(createPerson());
+        shopListController.create(shopList);
+
+        List<ShopList> shopListL = shopListDao.findAll();
+        assertEquals(shopListL.size(),1);
+
+        ShopList shopList1 = shopListL.get(0);
+        shopList1.setNote("new note");
+        shopListController.update(shopList1);
+
+
+        shopListL = shopListDao.findAll();
+        assertEquals(shopListL.get(0).getNote(),"new note");
     }
 
 
