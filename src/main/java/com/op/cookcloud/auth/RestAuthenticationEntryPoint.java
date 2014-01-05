2fb9dead00205fc7ce47f863f35ad9ed9f65eff6
raspad <kirill.torbin@gmail.com>
2014-01-05 21:01:36 +0200

    Rest authentication

---
+++ src/main/java/com/op/cookcloud/auth/RestAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.op.cookcloud.auth;
+
+import com.op.cookcloud.AppConstants;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author raspad
+ * @version 1.0 02.01.2013
+ */
+@Component(value="restAuthenticationEntryPoint")
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    /**
+     * Method rejects every unauthenticated request and send error code 401.
+     */
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse response,
+                         AuthenticationException e) throws IOException, ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, AppConstants.UNAUTHORIZED);
+    }
+}
---
+++ src/main/java/com/op/cookcloud/controller/AuthController.java
@@ -0,0 +1,60 @@
+package com.op.cookcloud.controller;
+
+import static com.op.cookcloud.AppConstants.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+
+/**
+ * @author raspad
+ * @version 1.0 04.01.2013
+ */
+@Controller
+@Path("/auth")
+//@RequestMapping("/auth")
+public class AuthController {
+
+    @Context private HttpServletRequest request;
+
+    @Context private HttpServletResponse response;
+
+    @POST
+    @Produces({MediaType.APPLICATION_JSON + CHARSET_UTF_8})
+    @Path("/login")
+    public @ResponseBody JSONObject login(final @QueryParam(value="auth") String auth)
+            throws JSONException, IOException {
+
+        JSONObject result = new JSONObject();
+        if (SUCCESS.equalsIgnoreCase(auth)) {
+            String sessionId = request.getSession().getId();
+            result.put(SESSION_ID, sessionId);
+            result.put(MESSAGE, "Successfully logged in");
+        } else if (FAILURE.equalsIgnoreCase(auth)) {
+            result.put(ERROR_CODE, HttpServletResponse.SC_UNAUTHORIZED);
+            result.put(MESSAGE, AUTHENTICATION_FAILURE);
+        } else {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Wrong url");
+        }
+
+        return result;
+    }
+    
+    @POST
+    @Produces({MediaType.APPLICATION_JSON + CHARSET_UTF_8})
+    @Path("/logout")
+    public @ResponseBody JSONObject logout() throws JSONException {
+        JSONObject result = new JSONObject();
+        result.put(MESSAGE, "Successfully logged out");
+        return result;
+    }
+}
